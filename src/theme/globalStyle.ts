4c81dccb0fb902e4451d923f8d32466fb334038a
mmehdinasiri <m.mehdi.nasiri@gmail.com>
2021-06-13 15:16:35 +0430

    add media queries sizes

--- src/theme/globalStyle.ts
+++ src/theme/globalStyle.ts
@@ -1,5 +1,20 @@
 import styled, { createGlobalStyle } from "styled-components";
 
+const size = {
+  mobile: "576px",
+  tablet: "768px",
+  laptop: "992px",
+  desktop: "1200px",
+  desktopL: "1400px",
+};
+export const device = {
+  mobile: `(min-width: ${size.mobile})`,
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  desktop: `(min-width: ${size.desktop})`,
+  desktopL: `(min-width: ${size.desktopL})`,
+};
+
 const GlobalStyle = createGlobalStyle`
 html {
 	scroll-behavior: smooth;
