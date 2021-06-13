b5d515ccf15a1f1f7ca8cd29a198fe7ffe46786f
mmehdinasiri <m.mehdi.nasiri@gmail.com>
2021-06-13 16:51:40 +0430

    get categories in inital of cats page

--- src/view/Cats.tsx
+++ src/view/Cats.tsx
@@ -1,7 +1,13 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { CatList, Layout } from "../components";
+import { useAppDispatch } from "../store";
+import { getCategories } from "../store/actions/category.action";
 
 const Cats: FC = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
   return (
     <Layout>
       <CatList />
