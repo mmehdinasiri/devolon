58a608bfb9ae2672df32cc8c52a4ab6c2e1cbb96
mmehdinasiri <m.mehdi.nasiri@gmail.com>
2021-06-13 17:14:13 +0430

    add cat services

---
+++ src/services/cats.services.ts
@@ -0,0 +1,12 @@
+import { AxiosRequestConfig } from "axios";
+import { GET_CAT_SEARCH } from "./urls";
+import axiosInstance from "./axiosInstance";
+
+export const getCatsService = (category: number, page = 1): Promise<ICat[]> => {
+  const limit = 10;
+  const reqConfig: AxiosRequestConfig = {
+    method: "GET",
+    url: `${GET_CAT_SEARCH}?limit=${limit}&category_ids=${category}&page=${page}`,
+  };
+  return axiosInstance(reqConfig).then((res) => res.data);
+};
